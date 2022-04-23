7716302ce595d0109b1a6b22b7cb7a567b83a9e7
kaushikchitraksha <87300983+kaushikchitraksha@users.noreply.github.com>
2022-04-23 23:10:37 +0530

    Add files via upload

---
+++ jUnitTest/src/jUnitTestPackage/jUnitFunctions.java
@@ -0,0 +1,17 @@
+//E2OCSE327_CHITRAKSHA
+package jUnitTestPackage;
+
+public class jUnitFunctions {
+	
+	public int addNumber(int a, int b) 
+	{
+		return a + b;
+	}
+	
+	
+	public String addString(String a, String b) 
+	{
+		String s1 = a.concat(b);
+		return s1;
+	}
+}
