a4ee699134cb00cfe781acca76f629aaf47f178f
kaushikchitraksha <87300983+kaushikchitraksha@users.noreply.github.com>
2022-04-23 22:58:12 +0530

    Add files via upload

---
+++ AllTests.java
@@ -0,0 +1,12 @@
+//E2OCSE327_CHITRAKSHA
+package jUnitTestPackage;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ testaddnumber.class, testaddstring.class })
+public class AllTests {
+
+}
---
+++ testaddstring.java
@@ -0,0 +1,17 @@
+//E2OCSE327_CHITRAKSHA
+package jUnitTestPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testaddstring {
+
+	@Test
+	public void test() {
+		jUnitFunctions junit = new jUnitFunctions();
+		String result = junit.addString("100", "200");
+		assertEquals("100200", result);
+	}
+
+}
